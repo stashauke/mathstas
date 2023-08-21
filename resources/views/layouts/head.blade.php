12d38292481d426975ba19a2978762866f678e9d
Peter Hauke <peter.hauke@lexikon.pl>
2023-08-21 20:04:15 +0200

    Refactor out blade layout parts

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Improve your maths skills by taking private online maths lessons with a very good maths graduate,
+               who will take you step by step through everything you need to get onto a good maths degree.">
+    <title>Private Maths Tuition</title>
+    <link rel="icon" type="image/x-icon" href="{{ url('favicon.ico') }}">
+
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
---
+++ resources/views/layouts/nav-header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav {!! isset($styles) ? 'style="'.$styles.'"' : null !!}>
+        <a href="/#home">Home</a>
+        <a href="/#maths">Maths</a>
+        <a href="/#who-are-you">Who are you?</a>
+        <a href="/#how-we-teach">How we teach?</a>
+        <a href="/#call-me">Call me!</a>
+        <a href="{{ url('pages') }}">Post</a>
+        <a href="{{ url('homepage') }}">List of Posts</a>
+    </nav>
+</header>
