c9c82e4e5f516d4b4b3031c11685565e107201b2
Peter Hauke <peter.hauke@lexikon.pl>
2023-08-21 21:28:17 +0200

    Add SVG logo

--- resources/views/layouts/nav-header.blade.php
+++ resources/views/layouts/nav-header.blade.php
@@ -1,5 +1,27 @@
 <header>
     <nav {!! isset($styles) ? 'style="'.$styles.'"' : null !!}>
+        <div style="width: 200px; height:200px; float:left;">
+            <svg viewBox="-50 -50 450 450">
+                <path d="M -15 -15 L -15 182 L 182 182 L 182 -15 L -15 -15 Z" fill="yellow" stroke="black" stroke-width="5">
+                </path>
+                <path d="M 0 0 L 0 96 L 96 96 L 96 0 L 0 0 Z" fill="transparent" stroke="teal" stroke-width="24">       </path>
+                <path d="M 0 72 L 0 168 L 96 168 L 96 72 L 0 72 Z" fill="transparent" stroke="blue" stroke-width="24">       </path>
+                <path d="M 72 72 L 72 168 L 168 168 L 168 72 L 72 72 Z" fill="transparent" stroke="crimson" stroke-width="24">       </path>
+                <path d="M 72 0 L 72 96 L 168 96 L 168 0 L 72 0 Z" fill="transparent" stroke="cyan" stroke-width="24">       </path>
+                <path d="M 0 59 L 0 84 Z" fill="transparent" stroke="teal" stroke-width="24">
+                </path>
+                <path d="M 72 84 L 72 108 Z" fill="transparent" stroke="teal" stroke-width="24">
+                </path>
+                <path d="M 71 -12 L 71 12 Z" fill="transparent" stroke="teal" stroke-width="24">
+                </path>
+                <path d="M 96 84 L 96 109 Z" fill="transparent" stroke="blue" stroke-width="24">
+                </path>
+                <path d="M 168 84 L 168 109 Z" fill="transparent" stroke="crimson" stroke-width="24">
+                </path>
+                <path d="M 72 156 L 72 180 Z" fill="transparent" stroke="blue" stroke-width="24">
+                </path>
+            </svg>
+        </div>
         <a href="/#home">Home</a>
         <a href="/#maths">Maths</a>
         <a href="/#who-are-you">Who are you?</a>
