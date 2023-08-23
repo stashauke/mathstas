2b1f64c4c50fef01fc17d1e427efae29cb6a4b0c
Peter Hauke <peter.hauke@lexikon.pl>
2023-08-23 08:21:50 +0200

    Add background colour and fix tailwind prose max-width issue

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('layouts.head')
-    <body class="antialiased bg-cyan-100">
+    <body class="antialiased">
     @include('layouts.nav-header', ['styles' => 'background: palegreen'])
     <main class="w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[60vw] min-h-[75vh] mx-auto">
-        <article class="prose prose-lg prose-p prose-h1:text-blue-600 prose-h2:text-green-600">
+        <article class="bg-pink-200 prose max-w-none prose-lg prose-p prose-h1:text-blue-600 prose-h2:text-green-600">
             {!! $post['slug'] !!}
             {!! $post['body'] !!}
         </article>
