94c4a9f8e26b498b19ba32dd2091a49635b467b4
Peter Hauke <peter.hauke@lexikon.pl>
2023-08-23 19:16:56 +0200

    Improve styling of pages

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -4,8 +4,7 @@
     <body class="antialiased">
     @include('layouts.nav-header', ['styles' => 'background: palegreen'])
     <main class="w-[90vw] md:w-[80vw] lg:w-[70vw] xl:w-[60vw] min-h-[75vh] mx-auto">
-        <article class="bg-pink-200 prose max-w-none prose-lg prose-p prose-h1:text-blue-600 prose-h2:text-green-600">
-            {!! $post['slug'] !!}
+        <article class="prose bg-white max-w-none prose-sm md:prose-md lg:prose-lg prose-h1:text-blue-600 prose-h2:text-green-600">
             {!! $post['body'] !!}
         </article>
     </main>
