ce2e54b33b4fe7960be6282b152ab23bbecc1b04
Peter Hauke <peter.hauke@lexikon.pl>
2023-08-23 15:29:03 +0200

    Fix menu to stop wrapping and turning into columns

--- resources/views/layouts/nav-header.blade.php
+++ resources/views/layouts/nav-header.blade.php
@@ -15,38 +15,38 @@
         data-te-navbar-ref>
         <div class="flex w-full flex-wrap items-center justify-between px-3">
             <!-- Hamburger button for mobile view -->
-            <button
-                class="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-                type="button"
-                data-te-collapse-init
-                data-te-target="#navbarSupportedContent1"
-                aria-controls="navbarSupportedContent1"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                <!-- Hamburger icon -->
-                <span class="[&>svg]:w-7">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="h-7 w-7">
-          <path
-              fill-rule="evenodd"
-              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-              clip-rule="evenodd" />
-        </svg>
-      </span>
-            </button>
+{{--            <button--}}
+{{--                class="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"--}}
+{{--                type="button"--}}
+{{--                data-te-collapse-init--}}
+{{--                data-te-target="#navbarSupportedContent1"--}}
+{{--                aria-controls="navbarSupportedContent1"--}}
+{{--                aria-expanded="false"--}}
+{{--                aria-label="Toggle navigation">--}}
+{{--                <!-- Hamburger icon -->--}}
+{{--                <span class="[&>svg]:w-7">--}}
+{{--        <svg--}}
+{{--            xmlns="http://www.w3.org/2000/svg"--}}
+{{--            viewBox="0 0 24 24"--}}
+{{--            fill="currentColor"--}}
+{{--            class="h-7 w-7">--}}
+{{--          <path--}}
+{{--              fill-rule="evenodd"--}}
+{{--              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"--}}
+{{--              clip-rule="evenodd" />--}}
+{{--        </svg>--}}
+{{--      </span>--}}
+{{--            </button>--}}
 
             <!-- Collapsible navigation container -->
             <div
-                class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+                class="flex-grow items-center !flex basis-auto"
                 id="navbarSupportedContent1"
                 data-te-collapse-item>
                 <!-- Logo -->
                 <a
                     class="mb-4 ml-2 mr-5 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
-                    href="#">
+                    href="/">
                 <div style="width:8vw; height:8vh; fill-opacity:0;">
                     <svg width="50" height="50" viewBox="-20 -20 220 220">
                         <path d="M -15 -15 L -15 182 L 182 182 L 182 -15 L -15 -15 Z" fill-opacity="1" fill="{{ $iconcolors['bg'] }}" stroke="black" stroke-width="5">
@@ -73,9 +73,9 @@
 
                 <!-- Left navigation links -->
                 <ul
-                    class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
+                    class="list-style-none mr-auto flex flex-wrap pl-0 flex-row"
                     data-te-navbar-nav-ref>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
                             href="/"
@@ -83,7 +83,7 @@
                         >Home</a
                         >
                     </li>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
                             href="/page/study"
@@ -91,7 +91,7 @@
                         >Studying at a University</a
                         >
                     </li>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                             href="/page/activities"
@@ -99,7 +99,7 @@
                         >Extracurricular activities</a
                         >
                     </li>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                             href="/page/teaching"
@@ -107,7 +107,7 @@
                         >How do I teach?</a
                         >
                     </li>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                             href="/page/contact"
@@ -115,7 +115,7 @@
                         >Get in touch!</a
                         >
                     </li>
-                    <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+                    <li class="mb-4 pr-2" data-te-nav-item-ref>
                         <a
                             class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                             href="/page/about"
